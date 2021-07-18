fb9bb842a107ac61021be1afed3095ab7b643383
philipwaluisachi <philipwaluisachi@gmail.com>
2021-07-18 18:48:00 -0400

    First commit

---
+++ cmake-build-debug/CMakeFiles/Reverse_shell.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Reverse_shell.dir/main.cpp.o"
+  "Reverse_shell"
+  "Reverse_shell.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Reverse_shell.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/Reverse_shell.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for Reverse_shell.
+# This may be replaced when dependencies are built.
